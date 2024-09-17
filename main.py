f5fb52efb0bfc6f6809f88b582bd295170ea57af
Jordi <55429631+jorbush@users.noreply.github.com>
2024-09-17 09:26:30 +0200

    finish fourth ex

--- main.py
+++ main.py
@@ -260,6 +260,41 @@ def random_forests():
     melb_preds = forest_model.predict(val_X)
     print(mean_absolute_error(val_y, melb_preds))
 
+def exercise_random_forests():
+    # Path of the file to read
+    iowa_file_path = './input/train.csv'
+    home_data = pd.read_csv(iowa_file_path)
+    # Create target object and call it y
+    y = home_data.SalePrice
+    # Create X
+    features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+    X = home_data[features]
+    # Split into validation and training data
+    train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+    # Specify Model
+    iowa_model = DecisionTreeRegressor(random_state=1)
+    # Fit Model
+    iowa_model.fit(train_X, train_y)
+    # Make validation predictions and calculate mean absolute error
+    val_predictions = iowa_model.predict(val_X)
+    val_mae = mean_absolute_error(val_predictions, val_y)
+    print("Validation MAE when not specifying max_leaf_nodes: {:,.0f}".format(val_mae))
+    # Using best value for max_leaf_nodes
+    iowa_model = DecisionTreeRegressor(max_leaf_nodes=100, random_state=1)
+    iowa_model.fit(train_X, train_y)
+    val_predictions = iowa_model.predict(val_X)
+    val_mae = mean_absolute_error(val_predictions, val_y)
+    print("Validation MAE for best value of max_leaf_nodes: {:,.0f}".format(val_mae))
+
+    # Define the model. Set random_state to 1
+    rf_model = RandomForestRegressor(random_state=1)
+    # fit your model
+    rf_model.fit(train_X, train_y)
+    # Calculate the mean absolute error of your Random Forest model on the validation data
+    rf_val_mae = mean_absolute_error(val_y, rf_model.predict(val_X))
+    print("Validation MAE for Random Forest Model: {}".format(rf_val_mae))
+
+
 if __name__ == '__main__':
     # basic_data_exploration()
     # exercise_explore_your_data()
@@ -269,4 +304,5 @@ if __name__ == '__main__':
     # exercise_model_validation()
     # underfitting_and_overfitting()
     # exercise_underfitting_and_overfitting()
-    random_forests()
+    # random_forests()
+    exercise_random_forests()
