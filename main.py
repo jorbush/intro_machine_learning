799c76945732accfbd00c06b093c846db5fc779f
Jordi <55429631+jorbush@users.noreply.github.com>
2024-09-17 09:09:27 +0200

    underfitting and overfitting exercise done

--- main.py
+++ main.py
@@ -192,6 +192,51 @@ def underfitting_and_overfitting():
         my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
         print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" % (max_leaf_nodes, my_mae))
 
+def exercise_underfitting_and_overfitting():
+    # Path of the file to read
+    iowa_file_path = './input/train.csv'
+    home_data = pd.read_csv(iowa_file_path)
+    # Create target object and call it y
+    y = home_data.SalePrice
+    # Create X
+    features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+    X = home_data[features]
+    # Split into validation and training data
+    train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+    # Specify Model
+    iowa_model = DecisionTreeRegressor(random_state=1)
+    # Fit Model
+    iowa_model.fit(train_X, train_y)
+    # Make validation predictions and calculate mean absolute error
+    val_predictions = iowa_model.predict(val_X)
+    val_mae = mean_absolute_error(val_predictions, val_y)
+    print("Validation MAE: {:,.0f}".format(val_mae))
+
+    def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+        model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+        model.fit(train_X, train_y)
+        preds_val = model.predict(val_X)
+        mae = mean_absolute_error(val_y, preds_val)
+        return (mae)
+
+    candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+    current_mae = float('inf')
+    best_leaf_nodes = None
+    # Write loop to find the ideal tree size from candidate_max_leaf_nodes
+    for max_leaf_nodes in candidate_max_leaf_nodes:
+        my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+        print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" % (max_leaf_nodes, my_mae))
+        if my_mae < current_mae:
+            current_mae = my_mae
+            best_leaf_nodes = max_leaf_nodes
+    best_tree_size = best_leaf_nodes
+    print(best_tree_size)
+
+    # Fill in argument to make optimal size and uncomment
+    final_model = DecisionTreeRegressor(max_leaf_nodes=best_tree_size, random_state=0)
+    # fit the final model and uncomment the next two lines
+    final_model.fit(X, y)
+
 if __name__ == '__main__':
     # basic_data_exploration()
     # exercise_explore_your_data()
@@ -199,4 +244,5 @@ if __name__ == '__main__':
     # exercise_first_machine_learning_model()
     # model_validation()
     # exercise_model_validation()
-    underfitting_and_overfitting()
+    # underfitting_and_overfitting()
+    exercise_underfitting_and_overfitting()
