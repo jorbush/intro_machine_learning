36f5dd85bcb71fde1729b3c8f58fceedf444ce96
Jordi <55429631+jorbush@users.noreply.github.com>
2024-09-17 08:36:13 +0200

    finish third exercise

--- main.py
+++ main.py
@@ -139,9 +139,35 @@ def model_validation():
     val_predictions = melbourne_model.predict(val_X)
     print(mean_absolute_error(val_y, val_predictions))
 
+def exercise_model_validation():
+    # Path of the file to read
+    iowa_file_path = './input/train.csv'
+    home_data = pd.read_csv(iowa_file_path)
+    y = home_data.SalePrice
+    feature_columns = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+    X = home_data[feature_columns]
+    # Specify Model
+    iowa_model = DecisionTreeRegressor()
+    # Fit Model
+    iowa_model.fit(X, y)
+    print("First in-sample predictions:", iowa_model.predict(X.head()))
+    print("Actual target values for those homes:", y.head().tolist())
+    train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+    iowa_model = DecisionTreeRegressor(random_state=1)
+    iowa_model.fit(train_X, train_y)
+    # Predict with all validation observations
+    val_predictions = iowa_model.predict(val_X)
+    # print the top few validation predictions
+    print(val_predictions)
+    # print the top few actual prices from validation data
+    print(val_y.head())
+    val_mae = mean_absolute_error(val_y, val_predictions)
+    print(val_mae)
+
 if __name__ == '__main__':
     # basic_data_exploration()
     # exercise_explore_your_data()
     # first_machine_learning_model()
     # exercise_first_machine_learning_model()
-    model_validation()
+    # model_validation()
+    exercise_model_validation()
