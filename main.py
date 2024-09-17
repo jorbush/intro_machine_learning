db3b6009f6613f2e0635c0a007b268b097c18adf
Jordi <55429631+jorbush@users.noreply.github.com>
2024-09-17 09:17:07 +0200

    random forests

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
@@ -237,6 +238,28 @@ def exercise_underfitting_and_overfitting():
     # fit the final model and uncomment the next two lines
     final_model.fit(X, y)
 
+def random_forests():
+    # The random forest uses many trees, and it makes a prediction by averaging the predictions of each component tree.
+    # It generally has much better predictive accuracy than a single decision tree and it works well with default
+    # parameters. If you keep modeling, you can learn more models with even better performance, but many of those are
+    # sensitive to getting the right parameters
+    melbourne_file_path = './input/melb_data.csv'
+    melbourne_data = pd.read_csv(melbourne_file_path)
+    # Filter rows with missing values
+    melbourne_data = melbourne_data.dropna(axis=0)
+    # Choose target and features
+    y = melbourne_data.Price
+    melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea',
+                          'YearBuilt', 'Lattitude', 'Longtitude']
+    X = melbourne_data[melbourne_features]
+    train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+    # We build a random forest model similarly to how we built a decision tree in scikit-learn - this time using
+    # the RandomForestRegressor class instead of DecisionTreeRegressor.
+    forest_model = RandomForestRegressor(random_state=1)
+    forest_model.fit(train_X, train_y)
+    melb_preds = forest_model.predict(val_X)
+    print(mean_absolute_error(val_y, melb_preds))
+
 if __name__ == '__main__':
     # basic_data_exploration()
     # exercise_explore_your_data()
@@ -245,4 +268,5 @@ if __name__ == '__main__':
     # model_validation()
     # exercise_model_validation()
     # underfitting_and_overfitting()
-    exercise_underfitting_and_overfitting()
+    # exercise_underfitting_and_overfitting()
+    random_forests()
