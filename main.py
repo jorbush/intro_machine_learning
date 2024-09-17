5e4033fdaa82560d7bac62a2a005542d318d7022
Jordi <55429631+jorbush@users.noreply.github.com>
2024-09-17 08:54:11 +0200

    underfitting and overfitting

--- main.py
+++ main.py
@@ -164,10 +164,39 @@ def exercise_model_validation():
     val_mae = mean_absolute_error(val_y, val_predictions)
     print(val_mae)
 
+def underfitting_and_overfitting():
+    # This is a phenomenon called overfitting, where a model matches the training data almost perfectly,
+    # but does poorly in validation and other new data.
+    # At an extreme, if a tree divides houses into only 2 or 4, each group still has a wide variety of houses.
+    # Resulting predictions may be far off for most houses, even in the training data (and it will be bad in
+    # validation too for the same reason). When a model fails to capture important distinctions and patterns
+    # in the data, so it performs poorly even in training data, that is called underfitting.
+
+    def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+        model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+        model.fit(train_X, train_y)
+        preds_val = model.predict(val_X)
+        mae = mean_absolute_error(val_y, preds_val)
+        return (mae)
+
+    melbourne_file_path = './input/melb_data.csv'
+    melbourne_data = pd.read_csv(melbourne_file_path)
+    filtered_melbourne_data = melbourne_data.dropna(axis=0)
+    y = filtered_melbourne_data.Price
+    melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea',
+                          'YearBuilt', 'Lattitude', 'Longtitude']
+    X = filtered_melbourne_data[melbourne_features]
+    train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+    # compare MAE with differing values of max_leaf_nodes
+    for max_leaf_nodes in [5, 50, 500, 5000]:
+        my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+        print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" % (max_leaf_nodes, my_mae))
+
 if __name__ == '__main__':
     # basic_data_exploration()
     # exercise_explore_your_data()
     # first_machine_learning_model()
     # exercise_first_machine_learning_model()
     # model_validation()
-    exercise_model_validation()
+    # exercise_model_validation()
+    underfitting_and_overfitting()
